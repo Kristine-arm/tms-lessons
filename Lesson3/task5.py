785e23efd615eef5dde7c0f39837565ea2b6cf5e
Kristine Sarsyants <christinesargsyants@gmail.com>
2023-09-22 19:11:11 +0200

    homework day3

---
+++ Lesson3/task5.py
@@ -0,0 +1,4 @@
+x=input("enter the month   ")
+print(x)
+y={'January': 31, 'February': 28,'March':31, 'April': 30, 'May': 31, 'June':30, 'July':31, 'August':31, 'September': 30, 'October':31, 'November': 30, 'december': 31 }
+print(y.get(x))
