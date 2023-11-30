950f7bc2692fb215005433e37cc41f32b9d6583e
Kristine Sarsyants <christinesargsyants@gmail.com>
2023-11-30 15:40:44 +0100

    Selenium tests

---
+++ test_main.py
@@ -0,0 +1,37 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture(autouse=True)
+def driver():
+    with webdriver.Chrome() as driver:
+        yield driver
+    ...
+    # driver = webdriver.Chrome()
+    #return webdriver.Chrome()
+
+
+class TestSelenium:
+    def test_selenium_first(self):
+        #Arrange
+        #driver = webdriver.Chrome()
+
+        # Act
+        driver.get("https://www.python.org/")
+        current_url= driver.current_url
+        page_title = driver.title
+        ...
+
+        # Assert
+        assert "python" in current_url
+
+
+    def test_selenium_second(self):
+        #Arrange
+        driver = webdriver.Chrome()
+
+        # Act
+        driver.get("https://www.python.org/downloads")
+        current_url= driver.current_url
+
+        # Assert
+        assert "downloads" in current_url
